e04e5a637c7eef0e9107e2b9f7a7cf4187beb32d
Krunoslav Husak <kruno@binel.hr>
2018-04-19 00:49:18 +0200

    Added logger

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/h00s/todo-api/logger"
 )
 
 func main() {
+	l, err := logger.New("todo-api.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+
 	r := gin.Default()
 	v1 := r.Group("/v1")
 	{
@@ -13,5 +22,6 @@ func main() {
 		})
 	}
 
+	l.Info("Starting server..")
 	r.Run()
 }
